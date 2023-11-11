f28395e472e27bca28b97edecae50fe8ca6a72ff
JDScript <jd@jdscript.net>
2023-11-11 19:23:29 +0800

    fix: activity table for mobile device

--- src/pages/activities/index.tsx
+++ src/pages/activities/index.tsx
@@ -37,9 +37,7 @@ const ActivitiesPage = () => {
   );
 
   return (
-    <Layout.Content
-      style={{ padding: 24, display: "flex", flexDirection: "column" }}
-    >
+    <Layout.Content style={{ padding: 24, width: "100%", overflow: "hidden" }}>
       <Typography.Title>
         ⏱️ Here are your activities in the system, {user?.name}!
       </Typography.Title>
@@ -86,62 +84,70 @@ const ActivitiesPage = () => {
           },
         ]}
       />
-      <Table
-        dataSource={data?.list}
-        pagination={{
-          currentPage: pagination.current,
-          pageSize: pagination.pageSize,
-          total: pagination.total,
-          showTotal: true,
-          showSizeChanger: false,
-          onChange(currentPage, pageSize) {
-            pagination.onChange(currentPage, pageSize);
-          },
-        }}
-        loading={loading}
-        columns={[
-          { dataIndex: "id", title: "ID", width: 100 },
-          { dataIndex: "type", title: "Type", width: 200 },
-          {
-            dataIndex: "method",
-            title: "Method",
-            render: (_, act) => (
-              <Tag color={REQUEST_METHOD_MAP[act.method]}>{act.method}</Tag>
-            ),
-            width: 100,
-          },
-          {
-            dataIndex: "path",
-            title: "Path",
-            ellipsis: { showTitle: false },
-            render: (text) => (
-              <Typography.Text ellipsis={{ showTooltip: true }}>
-                {text}
-              </Typography.Text>
-            ),
-          },
-          {
-            dataIndex: "header",
-            title: "Header",
-            ellipsis: { showTitle: false },
-            render: (text) => (
-              <Typography.Text ellipsis={{ showTooltip: true }}>
-                {JSON.stringify(text)}
-              </Typography.Text>
-            ),
-          },
-          { dataIndex: "src_ip", title: "Source IP" },
-          {
-            dataIndex: "created_at",
-            title: "Record Time",
-            render: (_, act) => (
-              <span style={{ fontVariant: "tabular-nums" }}>
-                {dayjs(act.created_at * 1000).format("YYYY-MM-DD HH:mm:ss Z")}
-              </span>
-            ),
-          },
-        ]}
-      />
+      <div style={{ overflowX: "scroll" }}>
+        <div style={{ minWidth: 1200 }}>
+          <Table
+            rowKey={"id"}
+            size="small"
+            dataSource={data?.list}
+            pagination={{
+              currentPage: pagination.current,
+              pageSize: pagination.pageSize,
+              total: pagination.total,
+              showTotal: true,
+              showSizeChanger: false,
+              onChange(currentPage, pageSize) {
+                pagination.onChange(currentPage, pageSize);
+              },
+            }}
+            loading={loading}
+            columns={[
+              { dataIndex: "id", title: "ID", width: 100 },
+              { dataIndex: "type", title: "Type", width: 200 },
+              {
+                dataIndex: "method",
+                title: "Method",
+                render: (_, act) => (
+                  <Tag color={REQUEST_METHOD_MAP[act.method]}>{act.method}</Tag>
+                ),
+                width: 100,
+              },
+              {
+                dataIndex: "path",
+                title: "Path",
+                ellipsis: { showTitle: false },
+                render: (text) => (
+                  <Typography.Text ellipsis={{ showTooltip: true }}>
+                    {text}
+                  </Typography.Text>
+                ),
+              },
+              {
+                dataIndex: "header",
+                title: "Header",
+                ellipsis: { showTitle: false },
+                render: (text) => (
+                  <Typography.Text ellipsis={{ showTooltip: true }}>
+                    {JSON.stringify(text)}
+                  </Typography.Text>
+                ),
+              },
+              { dataIndex: "src_ip", title: "Source IP" },
+              {
+                dataIndex: "created_at",
+                title: "Record Time",
+                render: (_, act) => (
+                  <span style={{ fontVariant: "tabular-nums" }}>
+                    {dayjs(act.created_at * 1000).format(
+                      "YYYY-MM-DD HH:mm:ss Z"
+                    )}
+                  </span>
+                ),
+              },
+            ]}
+          />
+        </div>
+      </div>
     </Layout.Content>
   );
 };
